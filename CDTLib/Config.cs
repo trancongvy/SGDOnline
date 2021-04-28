6b39deb974489de219ecfbc30dedca91619057e0
vy trần <trancongvy@gmail.com>
2021-04-28 08:44:04 +0700

    Upload All

--- CDTLib/Config.cs
+++ CDTLib/Config.cs
@@ -45,4 +45,13 @@ namespace CDTLib
             return (_variables[key]);
         }
     }
+    public partial class UserConnection
+    {
+        public string DatabaseName { get; set; }
+        public string ComputerName { get; set; }
+        public string LicenceKey { get; set; }
+        public string StructDb { get; set; }
+        public Nullable<System.DateTime> TimeEx { get; set; }
+        public int stt { get; set; }
+    }
 }
